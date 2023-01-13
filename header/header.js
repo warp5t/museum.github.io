eff6377536c035954f3bd8ca889a4f96d5e126bc
warp5t <dmitrij.didencko@yandex.by>
2023-01-13 17:55:54 +0300

    feat: implement adaptive header block, animation burger-menu

---
+++ header/header.js
@@ -0,0 +1,14 @@
+let burgerLine_1 = document.querySelector('.header__burger-line-1'),
+    burgerLine_2 = document.querySelector('.header__burger-line-2'),
+    burgerLine_3 = document.querySelector('.header__burger-line-3');
+
+let burgerMenu = document.querySelector('.header__wrap-burger');
+
+burgerMenu.onclick = burgerEngine;
+
+function burgerEngine(){
+  burgerMenu.classList.toggle('header__wrap-burger_line');
+  burgerLine_1.classList.toggle('header__burger-line-1_angle');
+  burgerLine_2.classList.toggle('header__burger-line-2_angle');
+  burgerLine_3.classList.toggle('header__burger-line_del');
+}    
