e9cbc42a09c5e1945807281032973781c9d471cf
warp5t <dmitrij.didencko@yandex.by>
2023-01-13 18:02:57 +0300

    refactor: delete unnesessary logic

--- header/header.js
+++ header/header.js
@@ -7,7 +7,6 @@ let burgerMenu = document.querySelector('.header__wrap-burger');
 burgerMenu.onclick = burgerEngine;
 
 function burgerEngine(){
-  burgerMenu.classList.toggle('header__wrap-burger_line');
   burgerLine_1.classList.toggle('header__burger-line-1_angle');
   burgerLine_2.classList.toggle('header__burger-line-2_angle');
   burgerLine_3.classList.toggle('header__burger-line_del');
