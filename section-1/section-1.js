11e0badb162026c4e2be2cd0cb52ccef2267d90b
warp5t <dmitrij.didencko@yandex.by>
2023-01-18 21:15:03 +0300

    fix: add function for resize swipe-surface/ relayout section-1 , header

--- section-1/section-1.js
+++ section-1/section-1.js
@@ -12,17 +12,19 @@ wrapRightArrow.onmouseover = mouseOver_R;
 wrapRightArrow.onmouseout = mouseOut_R;
 
 function mouseOver_L() {
-  leftArrow.src = "section-1/left-recolor.png";
+	leftArrow.src = "section-1/left-recolor.png";
 }
+
 function mouseOut_L() {
-  leftArrow.src = "section-1/arrow-left.png";
+	leftArrow.src = "section-1/arrow-left.png";
 }
 
 function mouseOver_R() {
-  rightArrow.src = "section-1/right-recolor.png";
+	rightArrow.src = "section-1/right-recolor.png";
 }
+
 function mouseOut_R() {
-  rightArrow.src = "section-1/arrow-right.png";
+	rightArrow.src = "section-1/arrow-right.png";
 }
 
 // -------------------------------------  carousel-pictures  ------------------------------------
@@ -32,7 +34,9 @@ let isEnabled = true;
 
 function changeCurrentItem(n) {
 	currentItem = (n + collectionPictures.length) % collectionPictures.length;
-	 console.log({currentItem});
+	console.log({
+		currentItem
+	});
 	recoloringItemClick();
 }
 
@@ -40,7 +44,7 @@ function hideItem(direction) {
 	isEnabled = false;
 	changingCount();
 	collectionPictures[currentItem].classList.add(direction);
-	collectionPictures[currentItem].addEventListener('animationend', function() {
+	collectionPictures[currentItem].addEventListener('animationend', function () {
 		this.classList.remove('active', direction);
 	});
 }
@@ -48,7 +52,7 @@ function hideItem(direction) {
 function showItem(direction) {
 	changingCount();
 	collectionPictures[currentItem].classList.add('next', direction);
-	collectionPictures[currentItem].addEventListener('animationend', function() {
+	collectionPictures[currentItem].addEventListener('animationend', function () {
 		this.classList.remove('next', direction);
 		this.classList.add('active');
 		isEnabled = true;
@@ -67,14 +71,14 @@ function previousItem(n) {
 	showItem('from-left');
 }
 
-wrapLeftArrow.addEventListener('click', function() {
+wrapLeftArrow.addEventListener('click', function () {
 	if (isEnabled) {
 		indexGraficItem = currentItem;
 		previousItem(currentItem);
 	}
 });
 
-wrapRightArrow.addEventListener('click', function() {
+wrapRightArrow.addEventListener('click', function () {
 	if (isEnabled) {
 		indexGraficItem = currentItem;
 		nextItem(currentItem);
@@ -97,19 +101,19 @@ const swipedetect = (el) => {
 	let restraint = 100;
 	let allowedTime = 300;
 
-	surface.addEventListener('mousedown', function(e){
+	surface.addEventListener('mousedown', function (e) {
 		startX = e.pageX;
 		startY = e.pageY;
 		startTime = new Date().getTime();
 		e.preventDefault();
 	}, false);
 
-	surface.addEventListener('mouseup', function(e){
+	surface.addEventListener('mouseup', function (e) {
 		distX = e.pageX - startX;
 		distY = e.pageY - startY;
 		elapsedTime = new Date().getTime() - startTime;
-		if (elapsedTime <= allowedTime){
-			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+		if (elapsedTime <= allowedTime) {
+			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
 				if ((distX > 0)) {
 					if (isEnabled) {
 						previousItem(currentItem);
@@ -124,69 +128,71 @@ const swipedetect = (el) => {
 		e.preventDefault();
 	}, false);
 
-	surface.addEventListener('touchstart', function(e){
-			var touchobj = e.changedTouches[0];
-			startX = touchobj.pageX;
+	surface.addEventListener('touchstart', function (e) {
+		var touchobj = e.changedTouches[0];
+		startX = touchobj.pageX;
 	}, false);
 
-	surface.addEventListener('touchend', function(e){
-			var touchobj = e.changedTouches[0];
-			distX = touchobj.pageX - startX;
-					if (Math.abs(distX) >= threshold){
-							if ((distX > 0)) {
-								if (isEnabled) {
-									previousItem(currentItem);
-								}
-							} else {
-								if (isEnabled) {
-									nextItem(currentItem);
-								}
-							}
-					}
+	surface.addEventListener('touchend', function (e) {
+		var touchobj = e.changedTouches[0];
+		distX = touchobj.pageX - startX;
+		if (Math.abs(distX) >= threshold) {
+			if ((distX > 0)) {
+				if (isEnabled) {
+					previousItem(currentItem);
+				}
+			} else {
+				if (isEnabled) {
+					nextItem(currentItem);
+				}
+			}
+		}
 	}, false);
 }
 
-var el = document.getElementById('surface-swipe');
-swipedetect(el);
+var surfaceSwipe = document.getElementById('surface-swipe');
+swipedetect(surfaceSwipe);
 
 // -------------------------------- grafic-counter-carousel --------------------
 var indexGraficItem = 0;
 var graficItem = document.querySelectorAll('.pannel-swipe__grafic-item');
-graficItem.forEach(function(item,index){
-	item.addEventListener('click',function(){
-	console.log({item},{index});
-	if(isEnabled){
-		indexGraficItem = index;
-	recoloring_swipe_button(indexGraficItem)
-	removingClass();
-	item.classList.add('recolor-active');
-	}
+graficItem.forEach(function (item, index) {
+	item.addEventListener('click', function () {
+		console.log({
+			item
+		}, {
+			index
+		});
+		if (isEnabled) {
+			indexGraficItem = index;
+			recoloring_swipe_button(indexGraficItem)
+			removingClass();
+			item.classList.add('recolor-active');
+		}
 	})
 });
 
-function removingClass(item){
-	graficItem.forEach(function(item){
+function removingClass(item) {
+	graficItem.forEach(function (item) {
 		item.classList.remove('recolor-active');
 	})
 }
 
-function recoloringItemClick(){
+function recoloringItemClick() {
 	removingClass();
 	graficItem[currentItem].classList.add('recolor-active');
 }
 
-function recoloring_swipe_button(indexGraficItem){
-	if(indexGraficItem > currentItem){
+function recoloring_swipe_button(indexGraficItem) {
+	if (indexGraficItem > currentItem) {
 		hideItem('to-left');
 		currentItem = indexGraficItem;
 		showItem('from-right');
-	}
-	else if(indexGraficItem < currentItem){
+	} else if (indexGraficItem < currentItem) {
 		hideItem('to-right');
 		currentItem = indexGraficItem;
 		showItem('from-left');
-	}
-	else if(indexGraficItem == currentItem){
+	} else if (indexGraficItem == currentItem) {
 
 	}
 }
@@ -195,7 +201,7 @@ function recoloring_swipe_button(indexGraficItem){
 
 let currentCount = document.querySelector('.pannel-swipe__progress-counter');
 
-function changingCount(){
+function changingCount() {
 	currentCount.innerText = '0' + (currentItem + 1);
 	console.log('currentItem : ', currentItem);
 }
@@ -205,23 +211,36 @@ function changingCount(){
 var welcomeInfo_1920 = document.querySelector('.welcome__version-1920px');
 var welcomeInfo_1024 = document.querySelector('.welcome__version-1024px');
 let switch_1024 = true;
-function slidingSide(){
-	if(switch_1024){
+
+function slidingSide() {
+	if (switch_1024) {
 		welcomeInfo_1920.classList.toggle('welcome__info-hide-1920');
-	setTimeout(delayingSlide,500);
-	}
-	else {
+		setTimeout(delayingSlide, 500);
+	} else {
 		welcomeInfo_1024.classList.toggle('welcome__info-show-1024');
-		setTimeout(delayingSlide,500);
+		setTimeout(delayingSlide, 500);
 	}
 }
-function delayingSlide(){
-	if(switch_1024){
+
+function delayingSlide() {
+	if (switch_1024) {
 		welcomeInfo_1024.classList.toggle('welcome__info-show-1024');
 		switch_1024 = false;
-	}
-	else{
+	} else {
 		welcomeInfo_1920.classList.toggle('welcome__info-hide-1920');
 		switch_1024 = true;
 	}
 }
+
+// ---------------------- resizing surface-swipe -------------------------
+let gallerySize = document.querySelector('.welcome__gallery');
+
+function resizingSurfaceSwipe() {
+	setTimeout(function () {
+			surfaceSwipe.style.width = gallerySize.offsetWidth + 'px';
+		surfaceSwipe.style.height = gallerySize.offsetHeight + 'px';
+		console.log('resizing');
+		console.log('|     |');
+	}, 1000);
+}
+window.addEventListener('resize', resizingSurfaceSwipe);
