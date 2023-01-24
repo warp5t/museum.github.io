02b2c6cb446ac2b5195f2cd1a8284ace4644b71d
warp5t <dmitrij.didencko@yandex.by>
2023-01-24 11:16:26 +0300

    feat: implement interactive cards

---
+++ section-2/section-2.js
@@ -0,0 +1,15 @@
+// ---------------------------- longing line decor ------------------------------
+let cardTour = document.querySelectorAll('.card-tour__wrap');
+let lineDecor_0 = document.querySelectorAll('.card-tour__line-decor');
+
+cardTour.forEach(function(item,index){
+  item.addEventListener('mouseover',function(){
+    lineDecor_0[index].classList.add('longing-line');
+    console.log('longing');
+  })
+  item.addEventListener('mouseout',function(){
+    lineDecor_0[index].classList.remove('longing-line');
+    console.log('longing');
+  })
+})
+
