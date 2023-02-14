4a26ed9ef13d6ba6f2a379de7de3227427095d22
warp5t <dmitrij.didencko@yandex.by>
2023-02-14 17:25:41 +0300

    refactor: change logic engine carousel section-4

--- section-4/section-4.js
+++ section-4/section-4.js
@@ -396,6 +396,7 @@ let videoList = document.querySelector('.video__list-video');
 let dotItems = document.querySelectorAll('.video__dot');
 
 let delayTime = 700;
+let indexVideo = 0;
 
 let videoContainer = {
   videoSrc_1: "section-4/video0.mp4",
@@ -416,76 +417,21 @@ videoPlayer.poster = videoContainer.poster_2;
 videoYouTube[0].style.backgroundColor = 'red';
 
 let counterSlide = 0;
-let marginCommon = 42;
-let result,previousResult = 0;
-let direction,sizeStep;
-
-function xMoving() {
-  sizeStep = ((videoYouTube[1].offsetWidth + marginCommon));
-  result = sizeStep * counterSlide;
-  videoYouTube.forEach(function (e) {
-    if(direction == 'right'){
-       e.style.right = result + 'px';
-    }
-    else if(direction == 'left' && counterSlide > 0){
-      e.style.right = result + 'px';
-    }
-    else if(direction == 'left' && counterSlide <= 0){
-      e.style.left = result*(-1) + 'px';
-    }
-  });
-  padding();
-  console.log('xMoving');
-}
-
-function padding() {
-  sizeStep = ((videoYouTube[1].offsetWidth + marginCommon));
-  result = sizeStep * counterSlide;
-  if (direction == 'right') {
-    setTimeout(function() {
-      videoList.style.paddingLeft = result + 'px';
-    }, delayTime);
-  } else if (direction == 'left') {
-      setTimeout(function() {
-        if(result < 0){result *=-1}
-      videoList.style.paddingLeft = result + 'px';
-      }, delayTime);
-  }
-}
-
-let indexVideo = 0;
-
-function floping() {
-  if (direction == 'right') {
-    termItem = videoYouTube[indexVideo];
-   // videoYouTube[indexVideo].remove();
-    videoList.append(termItem);
-    if (indexVideo >= 4) {
-      indexVideo = 0;
-    } else if (indexVideo >= 0) {
-      indexVideo++;
-    }
-  } else if (direction == 'left') {
-   // countItem = 4;
-   // while (countItem !== 0) {
-      if (indexVideo > 0) {
-        indexVideo--;
-      } else if (indexVideo == 0) {
-        indexVideo = 4;
-      }
-     // termItem = videoYouTube[indexVideo];
-     // videoYouTube[indexVideo].remove();
-     // termItem.style.left = result *(-1) + 'px';;
-    //  videoList.prepend(termItem);
-    //  countItem--;
-   //   console.log({countItem});
- //  }
-  }
-  console.log('floping');
-}
+let marginCommon = parseInt(window.getComputedStyle(videoYouTube[0], "").marginRight);
+let result;
+let direction, sizeStep;
 
 let switchDot = true;
 let switchIndexDot = 0;
+
+window.addEventListener('resize', () => {
+  marginCommon = parseInt(window.getComputedStyle(videoYouTube[0], "").marginRight);
+  stepCaclulating();
+});
+
+(function stepCaclulating() {
+  sizeStep = ((videoYouTube[1].offsetWidth + marginCommon));
+}())
 
 function dotRecoloring() {
   if (direction == 'right') {
@@ -514,9 +460,6 @@ function dotRecoloring() {
       dotItems[switchIndexDot].style.backgroundColor = '#333333';
       dotItems[switchIndexDot + 1].style.backgroundColor = '#999999';
     }
-    console.log({
-      switchIndexDot
-    });
   } else {
     dotItems[switchIndexDot].style.backgroundColor = '#333333';
   }
@@ -525,149 +468,79 @@ dotRecoloring();
 
 rightButton.onclick = function() {
   direction = 'right';
-  // counterSlide++;
-  // xMoving();
-  // setTimeout(function () {floping()}, delayTime)
-  testMoving_2();
-  setTimeout(function(){testMoving_1();},1000)
-  
+  counterSlide++;
+  rightMoving_1();
+  setTimeout(function() {
+    rightMoving_2()
+  }, 1000)
   dotRecoloring()
+  counterSlide--;
 }
-
-
 
 leftButton.onclick = function() {
   direction = 'left';
-  // if(counterSlide == 0){
-  //   videoYouTube.forEach(function(e){
-  //    e.style.right = 'unset';
-  //    e.style.left = '0';
-  //   })
-  // }
- // floping();
-  patternMoving()
- // counterSlide--;
-
-  //floping();
- // transitioning()
- // xMoving();
- // setTimeout(function() {}, delayTime);
-    dotRecoloring();
+  counterSlide--;
+  leftMoving_1();
+  setTimeout(function() {
+    leftMoving_2()
+  }, 10);
+  dotRecoloring();
+  counterSlide++;
 }
 
 
-
-
-function flipping(){
-  if(direction == 'left'){
-     counterSlide--;
-   if (indexVideo > 0){
-        indexVideo--;
-      } else if (indexVideo == 0) {
-        indexVideo = 4;
-      }
-  if(counterSlide < 0){
-    sizeStep = ((videoYouTube[1].offsetWidth + marginCommon));
-    result = sizeStep * counterSlide;
+function leftMoving_1() {
+  if (indexVideo > 0) {
+    indexVideo--;
+  } else if (indexVideo == 0) {
+    indexVideo = 4;
+  }
+  result = sizeStep * counterSlide;
   videoYouTube.forEach(function (e) {
-       e.style.transitionDuration = 'unset';
-       e.style.left = result  + 'px';
+    e.style.transitionDuration = 'unset';
+    e.style.left = result + 'px';
   });
   videoList.prepend(videoYouTube[indexVideo]);
-  }
-  counterSlide++;
-  }
-  else if(direction == 'right'){
-    counterSlide++;
-    if (indexVideo >= 4) {
-      indexVideo = 0;
-    } else if (indexVideo >= 0) {
-      indexVideo++;
-    }
-   if(counterSlide > 0){
-     sizeStep = ((videoYouTube[1].offsetWidth + marginCommon));
-     result = sizeStep * counterSlide;
-   videoYouTube.forEach(function (e) {
-        e.style.transitionDuration = '1s';
-        e.style.right = result  + 'px';
-   });
-   videoList.append(videoYouTube[indexVideo]);
-   }
-   counterSlide++;
-  }
 }
 
-function transitioning(){
-  if(direction == 'left'){
-    videoYouTube.forEach(function(e) {
+function leftMoving_2() {
+  videoYouTube.forEach(function (e) {
     e.style.transitionDuration = '1s';
-});
-videoYouTube.forEach(function(e){
-  e.style.right = 'unset';
-  e.style.left = '0px';
- })
-  }
-  else if(direction == 'right'){
-    videoYouTube.forEach(function(e) {
-      e.style.transitionDuration = 'unset';
   });
-  videoYouTube.forEach(function(e){
+  videoYouTube.forEach(function (e) {
     e.style.right = 'unset';
     e.style.left = '0px';
-   })
-  }
-// previousResult = result;
+  })
 }
 
-function patternMoving(){
- // counterSlide--;
-  flipping();
-  //counterSlide++;
-  setTimeout(function(){transitioning()},10);
-}
-
-function testMoving_0(){
-  counterSlide++;
-  sizeStep = ((videoYouTube[1].offsetWidth + marginCommon));
+function rightMoving_1() {
   result = sizeStep * counterSlide;
-  //videoYouTube.forEach(function(e){});
- // e.style.left = 'unset';
-  videoYouTube.forEach(function(e){
-    e.style.transitionDuration = '1s';
-    e.style.right = result + 'px';
-    e.style.left = 'unset';
-   })
-   counterSlide--;
-}
-
-function testMoving_1(){
-
   videoYouTube.forEach(function (e) {
-  e.style.transitionDuration = 'unset';
-  e.style.right = 'unset';
-  e.style.left = '0px';
-});
-videoList.append(videoYouTube[indexVideo]);
-if (indexVideo >= 4) {
-  indexVideo = 0;
-} else if (indexVideo >= 0) {
-  indexVideo++;
-}
-}
-
-function testMoving_2(){
-  videoYouTube.forEach(function(e){
     e.style.left = 'unset';
-   })
-   videoYouTube.forEach(function(e){
+  })
+  videoYouTube.forEach(function (e) {
     e.style.transitionDuration = '1s';
-   })
-    videoYouTube.forEach(function(e){
+  })
+  videoYouTube.forEach(function (e) {
     e.style.right = '0px';
-   })
-setTimeout(function(){
-   videoYouTube.forEach(function(e){
-    e.style.right = '494px';})
-   },10);
+  })
+  setTimeout(function () {
+    videoYouTube.forEach(function (e) {
+      e.style.right = result + 'px';
+    })
+  }, 10);
 }
 
+function rightMoving_2() {
+  videoYouTube.forEach(function (e) {
+    e.style.transitionDuration = 'unset';
+    e.style.right = 'unset';
+    e.style.left = '0px';
+  });
+  videoList.append(videoYouTube[indexVideo]);
+  if (indexVideo >= 4) {
+    indexVideo = 0;
+  } else if (indexVideo >= 0) {
+    indexVideo++;
+  }
+}
