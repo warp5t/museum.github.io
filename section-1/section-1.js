e5312fda32bdd4365515d16d2f4a232fe74faabf
warp5t <dmitrij.didencko@yandex.by>
2023-12-02 16:21:17 +0300

    fix: fix sidebar and section-4 response 1024px 768px

--- section-1/section-1.js
+++ section-1/section-1.js
@@ -1,9 +1,9 @@
 // ------------------------ recolor arrows -------------------------------
-let wrapLeftArrow = document.getElementById("wrap-left");
-let wrapRightArrow = document.getElementById("wrap-right");
+const wrapLeftArrow = document.getElementById("wrap-left");
+const wrapRightArrow = document.getElementById("wrap-right");
 
-let leftArrow = document.getElementById("left-arrow");
-let rightArrow = document.getElementById("right-arrow");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
 
 wrapLeftArrow.onmouseover = mouseOver_L;
 wrapLeftArrow.onmouseout = mouseOut_L;
@@ -28,7 +28,7 @@ function mouseOut_R() {
 }
 
 // -------------------------------------  carousel-pictures  ------------------------------------
-let collectionPictures = document.querySelectorAll('.welcome__picture');
+const collectionPictures = document.querySelectorAll('.welcome__picture');
 let currentItem = 0;
 let isEnabled = true;
 
@@ -97,9 +97,9 @@ const swipedetect = (el) => {
   let startTime = 0;
   let elapsedTime = 0;
 
-  let threshold = 150;
-  let restraint = 100;
-  let allowedTime = 300;
+  const threshold = 150;
+  const restraint = 100;
+  const allowedTime = 300;
 
   surface.addEventListener('mousedown', function (e) {
     startX = e.pageX;
@@ -195,7 +195,7 @@ function recoloring_swipe_button(indexGraficItem) {
 
 // -------------------------------- current-count-number --------------------------
 
-let currentCount = document.querySelector('.pannel-swipe__progress-counter');
+const currentCount = document.querySelector('.pannel-swipe__progress-counter');
 
 function changingCount() {
   currentCount.innerText = '0' + (currentItem + 1);
@@ -206,6 +206,11 @@ function changingCount() {
 
 var welcomeInfo_1920 = document.querySelector('.welcome__version-1920px');
 var welcomeInfo_1024 = document.querySelector('.welcome__version-1024px');
+const welcomeContRePadding = document.querySelector('.container-repadding-768');
+const welcomeCont = document.querySelector('.welcome__container');
+// welcomeCont.classList.add(welcomeContRePadding)
+
+
 let switch_1024 = true;
 
 function slidingSide() {
@@ -222,16 +227,18 @@ function delayingSlide() {
   if (switch_1024) {
     welcomeInfo_1024.classList.toggle('welcome__info-show-1024');
     switch_1024 = false;
+    // welcomeCont.style.padding = '0 2px 0px 20px';
   } else {
     welcomeInfo_1920.classList.toggle('welcome__info-hide-1920');
     switch_1024 = true;
+    // welcomeCont.style.padding = '0 20px 0px 20px';
   }
 }
 
 // ---------------------- resizing surface-swipe -------------------------
-let gallerySize = document.querySelector('.welcome__gallery');
-let body = document.getElementById('body');
-let surfaceSwipe_1 = document.getElementById('surface-swipe_1');
+const gallerySize = document.querySelector('.welcome__gallery');
+const body = document.getElementById('body');
+const surfaceSwipe_1 = document.getElementById('surface-swipe_1');
 
 function resizingSurfaceSwipe() {
   setTimeout(function () {
@@ -253,8 +260,8 @@ function resizingSurfaceSwipe() {
 window.addEventListener('resize', resizingSurfaceSwipe);
 
 // ------------------- gallery hide ------------------------------------
-let gallery = document.querySelector('.welcome__wrap-gallery');
-let controlPannelSwipe = document.querySelector('.welcome__control-pannel-swipe');
+const gallery = document.querySelector('.welcome__wrap-gallery');
+const controlPannelSwipe = document.querySelector('.welcome__control-pannel-swipe');
 
 function galleryHiding() {
   if (body.offsetWidth <= 1023) {
@@ -265,34 +272,37 @@ function galleryHiding() {
 
 // --------------------- adaptive height 768 slide ---------------------------
 
-let section_1_768 = document.querySelector('.welcome__container');
+const section_1_768 = document.querySelector('.welcome__container');
 let switcher_768 = true;
-let breakPoint_0 = {
+const breakPoint_0 = {
   width: 893,
   height: 949,
 };
-let breakPoint_1 = {
+const breakPoint_1 = {
   width: 470,
   height: 593,
 };
-let breakPoint_2 = {
+const breakPoint_2 = {
   width: 420,
   height: 543,
 };
 
 function heighting_768() {
   if (section_1_768.offsetWidth <= breakPoint_0.width && section_1_768.offsetWidth > breakPoint_1.width && switcher_768 == true) {
-    let difference = (breakPoint_0.width - section_1_768.offsetWidth) / 2;
+  
+  const  difference = (breakPoint_0.width - section_1_768.offsetWidth) / 2;
     section_1_768.style.height = breakPoint_0.height + 'px';
     section_1_768.style.height = (breakPoint_0.height - difference) + 'px';
     switcher_768 = false;
   } else if (section_1_768.offsetWidth <= breakPoint_1.width && section_1_768.offsetWidth > breakPoint_2.width && switcher_768 == true) {
-    let difference = (breakPoint_1.width - section_1_768.offsetWidth) / 2;
+  
+  const  difference = (breakPoint_1.width - section_1_768.offsetWidth) / 2;
     section_1_768.style.height = breakPoint_1.height + 'px';
     section_1_768.style.height = (breakPoint_1.height - difference) + 'px';
     switcher_768 = false;
   } else if (section_1_768.offsetWidth <= breakPoint_2.width && switcher_768 == true) {
-    let difference = (breakPoint_2.width - section_1_768.offsetWidth) / 2;
+  
+  const  difference = (breakPoint_2.width - section_1_768.offsetWidth) / 2;
     section_1_768.style.height = breakPoint_2.height + 'px';
     section_1_768.style.height = (breakPoint_2.height - difference) + 'px';
     switcher_768 = false;
@@ -304,9 +314,10 @@ function heighting_768() {
 //console.log(section_1_768.offsetHeight);
 
 // ----------------------- width line decor -----------------------------
-let lineDecor = document.querySelector('.welcome__line-decor');
-let gallery_768 = document.querySelector('.welcome__wrap-768');
-let gallery_420 = document.querySelector('.welcome__gallery-420');
+const lineDecor = document.querySelector('.welcome__line-decor');
+const welcomSocialNet = document.querySelector('.welcome__social-net-768');
+const gallery_768 = document.querySelector('.welcome__wrap-768');
+const gallery_420 = document.querySelector('.welcome__gallery-420');
 
 function widtingLineDecor() {
   if (body.offsetWidth <= 1023 && body.offsetWidth > 470) {
